9ab0154703f99f350eb1933e31b900828a66b2a0
Matthew Lee <ml8423799@gmail.com>
2022-06-14 17:13:10 +0700

    fifth commit

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import "./App.css";
 import Home from "./pages/Home";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import SearchPage from "./pages/SearchPage";
-
 function App() {
   return (
     <div className="app">
